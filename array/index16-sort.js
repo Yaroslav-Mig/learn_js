e91ef20cf0af2e13aeed07521f85b8e7dc177858
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2022-12-12 11:17:13 +0300

    feat: add method sort for array

---
+++ array/index16-sort.js
@@ -0,0 +1,38 @@
+// ! methods sort()
+
+//* The sort() method sorts the elements of an array
+//* The default sort order is ascending. Built upon converting the elements into strings, then comparing their sequences of UTF-16 code units values
+//* Returns the reference to the same array, now sorted.Note that the array is sorted in place, and no copy is made.
+//* If compareFn omitted, the array elements are converted to strings, then sorted according to each character's Unicode code point value.
+//* Empty slots are moved to the end of the array.
+
+// ? Syntax
+// array.sort()
+// array.sort((a, b) => { /* … */ } )
+// array.sort(compareFn)
+// a - The first element for comparison.
+// b - The second element for comparison.
+// compareFn - Specifies a function that defines the sort order.
+
+//TODO: sort()
+{
+  const months = ['March', 'Jan', 'Feb', 'Dec'];
+  console.log(months.sort());
+
+  const array1 = [1, 30, 4, 21, 100000];
+  console.log(array1.sort());
+
+  console.log([...array1].sort((a, b) => a - b));
+
+  const items = [
+    { name: 'Edward', value: 21 },
+    { name: 'Sharpe', value: 37 },
+    { name: 'And', value: 45 },
+    { name: 'The', value: -12 },
+    { name: 'Magnetic', value: 13 },
+    { name: 'Zeros', value: 37 },
+  ];
+
+  console.log(items.sort((a, b) => b.value - a.value));
+  console.log([...items].sort((a, b) => a.name > b.name ? 1 : -1));
+}
